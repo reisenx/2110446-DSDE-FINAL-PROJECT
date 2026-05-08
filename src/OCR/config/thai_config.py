4874aef315071a01d767533b7b973cda1efb6400
Worralop Srichainont <worralop.sric@gmail.com>
2026-05-09 02:31:30 +0700

    feat: add thai mappings

---
+++ src/OCR/config/thai_config.py
@@ -0,0 +1,42 @@
+class ThaiConfig:
+    THAI_PLACE_MAP = {
+        "บ้านธิ": "ban_thi",
+        "ห้วยยาบ": "huai_yap",
+        "เมืองลำพูน": "mueang_lamphun",
+        "ต้นธง": "ton_thong",
+        "บ้านกลาง": "ban_klang",
+        "มะเขือแจ้": "makhuea_chae",
+        "ริมปิง": "rim_ping",
+        "เวียงยอง": "wiang_yong",
+        "หนองหนาม": "nong_nam",
+        "เหมืองจี้": "mueang_chi",
+        "หนองช้างคืน": "nong_chang_khuen",
+        "ศรีบัวบาน": "si_bua_ban",
+        "ประตูป่า": "pratu_pa",
+        "ป่าสัก": "pa_sak",
+        "อุโมงค์": "umong",
+        "บ้านแป้น": "ban_paen",
+        "เหมืองง่า": "mueang_nga",
+        "แม่ทา": "mae_tha",
+        "ทาสบเส้า": "tha_sop_sao",
+        "ทาสบชัย": "tha_sop_chai",
+        "ทาปลาดุก": "tha_pla_duk",
+        "ทาทุ่งหลวง": "tha_thung_luang",
+        "ทากาศ": "tha_kat",
+        "ทาขุมเงิน": "tha_khum_ngoen",
+        "ทาแม่ลอบ": "tha_mae_lop",
+        "แพะริมน้ำ": "phae_rim_nam",
+    }
+
+    THAI_COMPONENTS_MAP = {
+        "อำเภอ": "district",
+        "ตำบล": "subdistrict",
+        "เทศบาล": "municipality",
+        "สนง.": "office",
+        "ทต.": "subdistrict_municipality",
+        "หน่วยเลือกตั้งที่": "polling_unit",
+        "ล่วงหน้าในเขต": "advance_in_district",
+        "ล่วงหน้านอกเขตเลือกตั้งและนอกราชอาณาจักร": "advance_outside_district_and_overseas",
+    }
+
+    PARTY_LIST_VOTE_KEYWORD = "(บช)"
