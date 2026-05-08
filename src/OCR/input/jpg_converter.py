90802752a96c479715d49f1b7295c15bb6bba3d1
Worralop Srichainont <worralop.sric@gmail.com>
2026-05-09 02:34:38 +0700

    feat: add pdf to jpg converter

---
+++ src/OCR/input/jpg_converter.py
@@ -0,0 +1,98 @@
+import pypdfium2
+from pathlib import Path
+from tqdm import tqdm
+from logs.logs import Logs
+from config.path_config import PathConfig
+
+
+class JPGConverter:
+    # Converter parameters
+    SCALE: float = 2.0
+    QUALITY: int = 90
+    COLOR_MODE: str = "RGB"
+    FILE_EXTENSION: str = "JPEG"
+
+    def get_all_jpg_from_pdf_file() -> None:
+        """
+        Converts all PDF files to JPG file
+        """
+
+        # Create output path
+        PathConfig.JPG_PATH.mkdir(parents=True, exist_ok=True)
+
+        # Write Logs
+        Logs.write_logs(messages=["Starting JPG file Converting Process"])
+        Logs.write_report(message="Starting JPG file Converting Process")
+
+        # Initialize pages counter
+        n_success = 0
+        n_total = 0
+
+        # Converts all PDf files into JPG files
+        all_files = list(PathConfig.PDF_PATH.rglob("*.pdf"))
+        for file_path in tqdm(all_files, desc="Converting PDFs to JPGs", unit="file"):
+            n_curr_success, n_curr_total = JPGConverter.get_jpg_from_pdf_file(file_path)
+
+            # Update counter
+            n_success += n_curr_success
+            n_total += n_curr_total
+
+        # Write Logs
+        n_skipped = n_total - n_success
+        Logs.write_logs(
+            messages=[
+                f"Done JPG file Converting Process ({n_success} pages converted and {n_skipped} pages skipped)"
+            ]
+        )
+        Logs.write_report(
+            message=f"Done JPG file Converting Process ({n_success} pages converted and {n_skipped} pages skipped)"
+        )
+
+    def get_jpg_from_pdf_file(file_path: Path) -> tuple[int, int]:
+        """
+        Converts a single PDF file to JPG file
+
+        Args:
+            file_path (Path): PDF file path
+
+        Returns:
+            tuple[int, int]: amount of successfully converted pages and total pages
+        """
+
+        # Construct a PDF file object
+        pdf_file = pypdfium2.PdfDocument(str(file_path))
+
+        # Get page count of the current PDF file
+        n_pages = len(pdf_file)
+        n_successful_pages = 0
+
+        # Iterate each page in a PDF file
+        for idx in range(n_pages):
+            # Get output directory of a current page
+            filename = f"{file_path.stem}_page_{idx + 1}.jpg"
+            output_dir = PathConfig.JPG_PATH / filename
+
+            # Skip if the current page is already exist
+            if output_dir.exists():
+                Logs.write_logs(
+                    messages=[f"Skipped {output_dir.name} because it is already exist"]
+                )
+                continue
+
+            # Converts to JPG file
+            page = pdf_file[idx]
+            bitmap = page.render(scale=JPGConverter.SCALE)
+            image = bitmap.to_pil().convert(JPGConverter.COLOR_MODE)
+            image.save(
+                output_dir, JPGConverter.FILE_EXTENSION, quality=JPGConverter.QUALITY
+            )
+
+            # Update counter
+            n_successful_pages += 1
+
+            # Write logs
+            Logs.write_logs(
+                messages=[f"Successfully convert {file_path.name} to {output_dir.name}"]
+            )
+
+        return (n_successful_pages, n_pages)
