e1d4e0a18abc61f607fec52a143e2218a5b88396
Worralop Srichainont <worralop.sric@gmail.com>
2026-05-09 12:00:08 +0700

    feat: implement ocr on main

--- src/OCR/main.py
+++ src/OCR/main.py
@@ -1,6 +1,7 @@
 from logs.logs import Logs
 from input.pdf_collector import PDFCollector
-from input.jpg_convertor import JPGConverter
+from input.jpg_converter import JPGConverter
+from model.thai_ocr import ThaiOCR
 
 
 class Main:
@@ -16,6 +17,9 @@ class Main:
         # Setup input files
         Main.setup_input_files()
 
+        # Process the files
+        Main.process_files()
+
     @staticmethod
     def setup_input_files():
         """
@@ -24,7 +28,16 @@ class Main:
         """
 
         PDFCollector.collect_all_pdf_files()
-        JPGConverter.get_all_jpg_from_pdf_file()
+        JPGConverter.get_all_jpg_from_pdf_files()
+
+    @staticmethod
+    def process_files():
+        """
+        Perform OCR on image files then cleaning them
+        """
+
+        thai_ocr = ThaiOCR()
+        thai_ocr.get_all_markdown_from_images()
 
 
 if __name__ == "__main__":
