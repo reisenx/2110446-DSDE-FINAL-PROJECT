452dcd9ce6ad87ea1a160bd10f6c806f2425aaca
Worralop Srichainont <worralop.sric@gmail.com>
2026-05-10 15:33:27 +0700

    feat: define more paths

--- src/OCR/config/path_config.py
+++ src/OCR/config/path_config.py
@@ -2,24 +2,41 @@ from pathlib import Path
 
 
 class PathConfig:
+    """
+    Path Configuration for the project. Every folder and file path
+    on this application must be declared here.
+    """
+
     # DEFINE RESOURCES PATHS
-    ROOT_PATH = Path(__file__).resolve().parent.parent.parent.parent
-    RAW_DATASET_PATH = ROOT_PATH / "data" / "raw"
+    ROOT_PATH: Path = Path(__file__).resolve().parent.parent.parent.parent
+    RAW_DATASET_PATH: Path = ROOT_PATH / "data" / "raw"
 
     # DEFINE OUTPUT PATHS
-    PDF_PATH = ROOT_PATH / "data" / "pdf"
-    JPG_PATH = ROOT_PATH / "data" / "jpg"
-    CSV_PATH = ROOT_PATH / "data" / "csv"
-    MARKDOWN_PATH = ROOT_PATH / "data" / "markdown"
+    PDF_PATH: Path = ROOT_PATH / "data" / "pdf"
+    JPG_PATH: Path = ROOT_PATH / "data" / "jpg"
+    MARKDOWN_PATH: Path = ROOT_PATH / "generated" / "markdown"
+    CSV_PATH: Path = ROOT_PATH / "generated" / "csv"
 
     # DEFINE LOGS PATHS
-    LOG_FOLDER_PATH = ROOT_PATH / "data" / "logs"
-    LOG_FILE_PATH = LOG_FOLDER_PATH / "log.txt"
-    REPORT_FILE_PATH = LOG_FOLDER_PATH / "report.txt"
+    LOG_FOLDER_PATH: Path = ROOT_PATH / "generated" / "logs"
+    LOG_FILE_PATH: Path = LOG_FOLDER_PATH / "log.txt"
+    REPORT_FILE_PATH: Path = LOG_FOLDER_PATH / "report.txt"
+
+    # DEFINE PARSER PATHS
+    PARSER_FOLDER_PATH: Path = ROOT_PATH / "generated" / "parser"
+
+    PARSER_INSPECT_FOLDER_PATH: Path = PARSER_FOLDER_PATH / "inspection"
+    PARSER_CSV_FOLDER_PATH: Path = PARSER_FOLDER_PATH / "csv"
+
+    BAD_PARTY_NAMES_FILE_PATH: Path = PARSER_INSPECT_FOLDER_PATH / "bad_party_names.csv"
+    BAD_NUMBER_TOKENS_FILE_PATH: Path = (
+        PARSER_INSPECT_FOLDER_PATH / "bad_number_tokens.csv"
+    )
+    VOTE_RESULTS_FILE_PATH: Path = PARSER_INSPECT_FOLDER_PATH / "vote_results.csv"
 
     # DEFINE EXCEPTIONS
-    EXCEPTION_FILENAMES = ["desktop.ini"]
-    OCR_EXCEPTION_FILENAMES = [
+    EXCEPTION_FILENAMES: list[str] = ["desktop.ini"]
+    OCR_EXCEPTION_FILENAMES: list[str] = [
         "district_mae_tha__02_subdistrict_municipality_tha_sop_chai__polling_unit_7__constituency_vote_page_2.jpg",
         "district_mueang_lamphun__01_subdistrict_ton_thong__polling_unit_1__constituency_vote_page_2.jpg",
         "district_mueang_lamphun__04_subdistrict_rim_ping__polling_unit_9__constituency_vote_page_2.jpg",
